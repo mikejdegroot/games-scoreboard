61d1106f3dbcfc9881b4b7c8421b6aa898667433
Mike de Groot <mike.j.degroot@gmail.com>
2017-10-02 12:32:27 +0100

    fe controller- to look up populate next

--- src/js/controllers/games.js
+++ src/js/controllers/games.js
@@ -1,62 +1,29 @@
 angular
-  .module('gameApp')
-  .controller('gamesIndexCtrl', gamesIndexCtrl)
-  .controller('gamesNewCtrl', gamesNewCtrl)
-  .controller('gamesShowCtrl', gamesShowCtrl)
-  .controller('gamesEditCtrl', gamesEditCtrl);
+.module('gameApp')
+.controller('gamesIndexCtrl', gamesIndexCtrl);
 
-gamesIndexCtrl.$inject = ['game'];
-function gamesIndexCtrl(game) {
-  const vm = this;
 
-  vm.all = game.query();
-}
-
-gamesNewCtrl.$inject = ['game', '$state'];
-function gamesNewCtrl(game, $state) {
+gamesIndexCtrl.$inject = ['game', '$state'];
+function gamesIndexCtrl(game, $state) {
   const vm = this;
   vm.game = {};
+  vm.all = game.query();
 
   function gamesCreate() {
-    if (vm.gameForm.$valid) {
-      game
-        .save(vm.game)
-        .$promise
-        .then(() => $state.go('gamesIndex'));
-    }
+    game
+    .save(vm.game)
+    .$promise
+    .then(() => $state.go('gamesIndex'));
+
   }
-
   vm.create = gamesCreate;
-}
 
-gamesShowCtrl.$inject = ['game', '$stateParams', '$state'];
-function gamesShowCtrl(game, $stateParams, $state) {
-  const vm = this;
-
-  vm.game = game.get($stateParams);
 
   function gamesDelete() {
     vm.game
-      .$remove()
-      .then(() => $state.go('gamesIndex'));
+    .$remove()
+    .then(() => $state.go('gamesIndex'));
   }
 
   vm.delete = gamesDelete;
-}
-
-gamesEditCtrl.$inject = ['game', '$stateParams', '$state'];
-function gamesEditCtrl(game, $stateParams, $state) {
-  const vm = this;
-
-  vm.game = game.get($stateParams);
-
-  function gamesUpdate() {
-    if (vm.gameForm.$valid) {
-      vm.game
-        .$update()
-        .then(() => $state.go('gamesShow', $stateParams));
-    }
-  }
-
-  vm.update = gamesUpdate;
 }
